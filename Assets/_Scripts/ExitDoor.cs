a044f747ae0982e3b2cd2926a8e1f011e3d7ca28
Felipe <felipe.mendez360@gmail.com>
2024-07-28 18:24:49 -0400

    WIP puerta salida

---
+++ Assets/_Scripts/ExitDoor.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitDoor : MonoBehaviour, IInteractable {
+    public bool isDoorEnabled = true;
+    public bool IsInteractable() {
+        return isDoorEnabled;
+    }
+
+    public void TriggerInteraction() {
+        if (!isDoorEnabled) return;
+        LoadNextLevel();
+    }
+    public void LoadNextLevel() {
+        print("PASASTE EL DIA MACACO");
+    }
+}
