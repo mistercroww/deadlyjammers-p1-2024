ebb69ff954cfb29c9808151de4f524a78e745e26
Felipe <felipe.mendez360@gmail.com>
2024-07-29 22:57:55 -0400

    main menu WIP

---
+++ Assets/_Scripts/MainMenu_Button.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MainMenu_Button : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+    public GameObject indicatorObj;
+    public float sideOffset = 25f;
+    
+    public void PlayGame() {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Gameplay");
+    }
+    public void ExitGame() {
+        Application.Quit();
+    }
+    public void OnPointerEnter(PointerEventData eventData) {
+        if (indicatorObj) {
+            indicatorObj.transform.position = transform.position + (Vector3.right * sideOffset);
+            indicatorObj.SetActive(true);
+        }
+    }
+    public void OnPointerExit(PointerEventData eventData) {
+        if (indicatorObj) {
+            indicatorObj.SetActive(false);
+        }
+    }
+}
